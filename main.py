4ac2328e93d57afa2a5c8295aaf8d8ffc8336a5a
MSchrodinger <williamyi@outlook.sa>
2020-05-20 17:36:12 +0300

    make it as class

--- main.py
+++ main.py
@@ -5,78 +5,87 @@ import re
 import datetime
 
 # import calendar
-json_file = open("data.json", "r")
-data = json.load(json_file)
-json_file.close()
 
 
-def if_exist(email):
-    global data
-    for i in range(len(data)):
-        if data[i]["Email"] == email:
-            return (i, True)
-
-    return (-1, False)
+# json_file = open("data.json", "r")
+# data = json.load(json_file)
+# json_file.close()
 
 
-def new_subscription(email, username, sub_days, price):
-    global data
-    date_now = datetime.datetime.now()
-    user_index, flag = if_exist(email)
-    if flag:
-        data[user_index]["Days"] += sub_days
-        date = re.split(r"[/: ]", data[user_index]["Ends at"])
-        new_date = datetime.datetime(
-            int(date[0]), int(date[1]), int(date[2]), int(date[3]), int(date[4])
-        )
-        new_date += datetime.timedelta(days=sub_days)
-        data[user_index]["Ends at"] = new_date.strftime("%Y/%m/%d %H:%M")
-        data[user_index]["Payments"].append((date_now.strftime("%Y/%m/%d"), price))
-    else:
-        date_expiration = date_now + datetime.timedelta(days=sub_days)
-        new_user = {
-            "Email": email,
-            "UserName": username,
-            "Days": sub_days,
-            "Started": date_now.strftime("%Y/%m/%d %H:%M"),
-            "Ends at": date_expiration.strftime("%Y/%m/%d %H:%M"),
-            "Payments": [(date_now.strftime("%Y/%m/%d"), price)],
-        }
-        data.append(new_user)
+class Subs_record:
+    def __init__(self, data_file_name):
+        self.data_file_name = data_file_name
+        self.data = json.load(open(data_file_name, "r"))
 
+    def if_exist(self, email):
+        for i in range(len(self.data)):
+            if self.data[i]["Email"] == email:
+                return (i, True)
 
-def delete_subscription(email):
-    global data
-    user_index, flag = if_exist(email)
-    if flag:
-        data.pop(user_index)
-    else:
-        print("User not exist!!")
+        return (-1, False)
 
+    def new_subscription(self, email, username, sub_days, price):
+        date_now = datetime.datetime.now()
+        user_index, flag = self.if_exist(email)
+        if flag:
+            self.data[user_index]["Days"] += sub_days
+            self.date = re.split(r"[/: ]", self.data[user_index]["Ends at"])
+            new_date = datetime.datetime(
+                int(self.date[0]),
+                int(self.date[1]),
+                int(self.date[2]),
+                int(self.date[3]),
+                int(self.date[4]),
+            )
+            new_date += datetime.timedelta(days=sub_days)
+            self.data[user_index]["Ends at"] = new_date.strftime("%Y/%m/%d %H:%M")
+            self.data[user_index]["Payments"].append(
+                (date_now.strftime("%Y/%m/%d"), price)
+            )
+        else:
+            date_expiration = date_now + datetime.timedelta(days=sub_days)
+            new_user = {
+                "Email": email,
+                "UserName": username,
+                "Days": sub_days,
+                "Started": date_now.strftime("%Y/%m/%d %H:%M"),
+                "Ends at": date_expiration.strftime("%Y/%m/%d %H:%M"),
+                "Payments": [(date_now.strftime("%Y/%m/%d"), price)],
+            }
+            self.data.append(new_user)
 
-def check_expire():
-    global data
-    soon = []
-    for i in range(len(data)):
-        # date_start = re.split(r"[/: ]", data[i]["Started"])
-        # s_ = datetime.datetime(
-        #     int(date_start[0]), int(date_start[1]), int(date_start[2]), int(date_start[3]), int(date_start[4])
-        # )
-        date_end = re.split(r"[/: ]", data[i]["Ends at"])
-        e_ = datetime.datetime(
-            int(date_end[0]),
-            int(date_end[1]),
-            int(date_end[2]),
-            int(date_end[3]),
-            int(date_end[4]),
-        )
+    def delete_subscription(self, email):
+        user_index, flag = self.if_exist(email)
+        if flag:
+            self.data.pop(user_index)
+        else:
+            print("User not exist!!")
 
-        n = str(e_ - datetime.datetime.now())
-        remaining = int(re.split(r"[/:, ]", n)[0])
-        if remaining < 3:
-            soon.append(data[i])
+    def check_expire(self):
+        self.soon = []
+        for i in range(len(self.data)):
+            # date_start = re.split(r"[/: ]", data[i]["Started"])
+            # s_ = datetime.datetime(
+            #     int(date_start[0]), int(date_start[1]), int(date_start[2]), int(date_start[3]), int(date_start[4])
+            # )
+            date_end = re.split(r"[/: ]", self.data[i]["Ends at"])
+            e_ = datetime.datetime(
+                int(date_end[0]),
+                int(date_end[1]),
+                int(date_end[2]),
+                int(date_end[3]),
+                int(date_end[4]),
+            )
 
-    return soon
+            n = str(e_ - datetime.datetime.now())
+            remaining = int(re.split(r"[/:, ]", n)[0])
+            if remaining < 3:
+                self.soon.append(data[i])
+
+        return self.soon
+
+    def write_json(self):
+        json.dump(self.data, open(self.data_file_name, "w"))
 
 
 # date_now = datetime.date.today()
@@ -87,14 +96,14 @@ def check_expire():
 
 
 # print(json.dumps(x, indent=2))
-def write_json(data):
-    with open("data.json", "w") as json_file:
-        json.dump(data, json_file)
 
 
 if __name__ == "__main__":
     # new_subscription("test@someting.com", "user1", 5,190)
-    s = check_expire()
-    print(s)
+    s = Subs_record("data.json")
+    s.new_subscription("test2@someting.com", "user2", 10, 100)
+    s.write_json()
+    print(json.dumps(s.data, indent=2))
+    print()
     # # delete_subscription("test@someting.com")
     # write_json(data)
